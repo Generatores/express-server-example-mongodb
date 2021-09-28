46142592e8656ba169e9f2324fa029d0e7f1d8fd
Generatores <bruno.ruiz@bbrr.solutions>
2021-09-28 13:26:24 -0700

    splitting of db connectio

--- server.js
+++ server.js
@@ -4,14 +4,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-const mongoose = require("mongoose");
-
 app.use(express.json());
 
-mongoose.connect(process.env.DATABASE_URL);
-const db = mongoose.connection;
-db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("Connection to DB successfull..."));
+const db = require("./db");
+db;
 
 const Users = require("./routes/users");
 app.use("/users", Users);
