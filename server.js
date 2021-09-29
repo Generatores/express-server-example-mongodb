da1b34f8cac7b5bba730c89019b44369ed4cd1a6
Generatores <bruno.ruiz@bbrr.solutions>
2021-09-29 16:52:49 -0700

    addition of http form

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 //* Declaration of MongoDB
